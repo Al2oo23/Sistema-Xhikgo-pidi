054179161f1bae770b54244c1fd251cc933fe050
Jaimaaa <jaimary1@outlook.com>
2024-03-07 21:44:44 -0400

    hola

--- SISTEMA/vista/footer.php
+++ SISTEMA/vista/footer.php
@@ -1,7 +1,7 @@
 <footer class="position-absolute bottom-0">
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2023 &copy; Mazer Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+            <p>2024 &copy; Mazer Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
                 by <a href="https://saugi.me">Saugi</a></p>
         </div>
         <div class="float-end">
