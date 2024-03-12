5c12c8abbeae3ac7b144b095bcd9a1c80675d24e
aldobluelabel <141055529+aldobluelabel@users.noreply.github.com>
2024-03-12 16:33:23 -0500

    pantallas-aldo

---
+++ SISTEMA/vista/marca.php
@@ -0,0 +1,38 @@
+<?php
+$nombrePagina="Marca";
+require('../header.php');
+
+?>
+
+<div class="col-md-6 col-12 m-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Registro de Marca de Vehiculos</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body">
+                            <form class="form form-vertical">
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Nombre de la Marca</label>
+                                                
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="Coloque el nombre de la marca del vehiculo" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                    <i class="bi bi-postcard"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-end">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Registrar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
---
+++ SISTEMA/vista/municipio.php
@@ -0,0 +1,38 @@
+<?php
+$nombrePagina="Municipio";
+require('../header.php');
+
+?>
+
+<div class="col-md-6 col-12 m-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Registro de Municipio</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body">
+                            <form class="form form-vertical">
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Nombre del Municipio</label>
+                                                
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="Coloque el nombre del municipio" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                    <i class="bi bi-shop"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-end">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Registrar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
